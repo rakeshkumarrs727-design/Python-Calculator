281a89ac9d4d2fda81b05cd8a6c852f25b1b71e2
rakeshkumarrs727-design <rakeshkumarrs727@gmail.com>
2026-03-06 00:37:12 +0530

    Create password_generator.py

---
+++ password_generator.py
@@ -0,0 +1,19 @@
+import random
+import string
+
+print("---- Password Generator ----")
+
+length = int(input("Enter password length: "))
+
+letters = string.ascii_letters
+digits = string.digits
+symbols = string.punctuation
+
+all_characters = letters + digits + symbols
+
+password = ""
+
+for i in range(length):
+    password += random.choice(all_characters)
+
+print("Generated Password:", password)
